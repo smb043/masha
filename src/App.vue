430762a0bc6108eed3f5e65f131b35610f8572e8
Pavel Bel <pavelbel@MacBook-Pro-Pavel.local>
2021-05-13 13:53:58 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>bla</h1>
+  </div>
+</template>
+
+<script>
+import Unicrypto from 'unicrypto'
+
+export default {
+  name: 'App',
+  created () {
+    this.initUnicrypto()
+  },
+  methods: {
+    async initUnicrypto () {
+      const options = { strength: 2048 }
+      const priv = await Unicrypto.PrivateKey.generate(options)
+      console.log(priv)
+    }
+  },
+}
+</script>
