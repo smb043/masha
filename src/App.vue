308cfc8ea6aeaa8ece4fa317ce85473451894037
Pavel Bel <pavelbel@MacBook-Pro-Pavel.local>
2021-05-17 11:46:09 +0300

    add uni.min

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,13 @@
 </template>
 
 <script>
-import Unicrypto from 'unicrypto'
-
 export default {
   name: 'App',
   created () {
-    this.initUnicrypto()
+    
   },
   methods: {
-    async initUnicrypto () {
-      const options = { strength: 2048 }
-      const priv = await Unicrypto.PrivateKey.generate(options)
-      console.log(priv)
-    }
+
   },
 }
 </script>
